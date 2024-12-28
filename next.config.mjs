fcd0b3e4f816bce7001ae58feb0724b3aa3a79fb
Adhil-Akbar <356adhil@gmail.com>
2024-12-28 21:56:37 +0530

    Enhance invoice generation by updating browser configuration and logging; implement temporary directory for Chromium cache and improve error handling.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        "puppeteer-core": "puppeteer-core",
+        "@sparticuz/chromium": "@sparticuz/chromium",
+      });
+    }
+    return config;
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
 
 export default nextConfig;
